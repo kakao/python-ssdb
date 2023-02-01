125b0e211df34c83e53406129955dbb1e059ee9d
dante.l <dante.l@kakaocorp.com>
2023-02-01 13:35:33 +0900

    change extension build

--- build_extension.py
+++ build_extension.py
@@ -1,40 +1,72 @@
-from setuptools import Extension
+import os
+import shutil
 
-ext_modules = [
-    Extension(
-        name="ssdb.core.parser",
-        sources=["./ssdb/core/parser.pyx"],
-    ),
-    Extension(
-        name="ssdb.core.encode",
-        sources=["./ssdb/core/encode.pyx"],
-    ),
-    Extension(
-        name="ssdb.core.response",
-        sources=["./ssdb/core/response.pyx"],
-    ),
-    Extension(
-        name="ssdb.core.interface",
-        sources=["./ssdb/core/interface.pyx"],
-    ),
-    Extension(
-        name="ssdb.client",
-        sources=["./ssdb/client.pyx"],
-    ),
-    Extension(
-        name="ssdb.connection",
-        sources=["./ssdb/connection.pyx"],
-    ),
-    Extension(
-        name="ssdb.asyncio.client",
-        sources=["./ssdb/asyncio/client.pyx"],
-    ),
-    Extension(
-        name="ssdb.asyncio.connection",
-        sources=["./ssdb/asyncio/connection.pyx"],
-    ),
-]
+from Cython.Build import cythonize
+from setuptools import Distribution, Extension
+from setuptools.command.build_ext import build_ext
 
 
-def build(kwargs):
-    kwargs.update({"ext_modules": ext_modules, "zip_safe": False})
+def build():
+    extra_compile_args = ["-O3"]
+
+    extensions = [
+        Extension(
+            name="ssdb.core.parser",
+            sources=["./ssdb/core/parser.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.core.encode",
+            sources=["./ssdb/core/encode.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.core.response",
+            sources=["./ssdb/core/response.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.core.interface",
+            sources=["./ssdb/core/interface.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.client",
+            sources=["./ssdb/client.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.connection",
+            sources=["./ssdb/connection.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.asyncio.client",
+            sources=["./ssdb/asyncio/client.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+        Extension(
+            name="ssdb.asyncio.connection",
+            sources=["./ssdb/asyncio/connection.pyx"],
+            extra_compile_args=extra_compile_args,
+        ),
+    ]
+
+    ext_modules = cythonize(extensions)
+
+    dist = Distribution({"name": "extended", "ext_modules": ext_modules})
+
+    cmd = build_ext(dist)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
