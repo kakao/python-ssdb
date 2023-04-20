6500b9f3166e02d7ef0ef7b475d1c9b42f299b7b
dante.l <dante.l@kakaocorp.com>
2023-04-20 12:25:36 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+
 from setuptools import Extension, find_packages, setup
 
 ext_modules = [
@@ -36,8 +38,32 @@ ext_modules = [
 ]
 
 
+def read(file_name):
+    """Read a text file and return the content as a string."""
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(file_path, encoding="utf-8") as f:
+        return f.read()
+
+
 setup(
+    name="python-ssdb",
+    version="1.0.4",
+    description="SSDB Python Client",
+    long_description=read("README.md"),
+    python_requires=">3.9",
+    author="recoteam",
+    author_email="recoteam@kakaocorp.com",
     url="https://github.com/kakao/python-ssdb",
+    license="Apache2",
+    keywords="SSDB",
+    classifiers=[
+        "License :: Apache Software License",
+        "Operating System :: Linux",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Cython",
+    ],
     packages=find_packages(include=["ssdb", "ssdb.core", "ssdb.asyncio"]),
     ext_modules=ext_modules,
 )
